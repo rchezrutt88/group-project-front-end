7f523861feba2c6ed20307842a0150c3461e5087
kpie89 <kpie89@gmail.com>
2016-03-11 16:09:36 -0500

    add basic sign-in AJAX

--- assets/scripts/user-actions.js
+++ assets/scripts/user-actions.js
@@ -4,28 +4,42 @@ const myApp = {
   BASE_URL: 'http://localhost:3000',
 };
 
-$('#sign-up').on('submit', function(e) {
+  let signUp = function(e) {
+    e.preventDefault();
+    let formData = new FormData(e.target);
+    $.ajax({
+      url: myApp.BASE_URL + '/sign-up',
+      method: 'POST',
+      contentType: false,
+      processData: false,
+      data: formData,
+    }).done(function(data) {
+      console.log(data);
+    }).fail(function(jqxhr) {
+      console.error(jqxhr);
+    });
+  };
+
+let signIn = function(e) {
+    e.preventDefault();
+    let formData = new FormData(e.target);
+    $.ajax({
+      url: myApp.BASE_URL + '/sign-in',
+      method: 'POST',
+      contentType: false,
+      processData: false,
+      data: formData,
+    }).done(function(data) {
+      console.log(data);
+    }).fail(function(jqxhr) {
+      console.error(jqxhr);
+    });
+  };
 
 
 
-let signUp = function(e) {
-  e.preventDefault();
-  let formData = new FormData(e.target);
-  $.ajax({
-    url: myApp.BASE_URL + '/sign-up',
-    method: 'POST',
-    contentType: false,
-    processData: false,
-    data: formData,
-  }).done(function(data) {
-    console.log(data);
-  }).fail(function(jqxhr) {
-    console.error(jqxhr);
-  });
- };
-});
 
-
-module.exports = {
-  signUp,
-};
+    module.exports = {
+      signUp,
+      signIn
+    };
