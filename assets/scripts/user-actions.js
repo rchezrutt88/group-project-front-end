39be051e58cd9fc19e010e617507a1e4ec6277d5
kpie89 <kpie89@gmail.com>
2016-03-11 16:04:07 -0500

    add basic sign-up AJAX

--- assets/scripts/user-actions.js
+++ assets/scripts/user-actions.js
@@ -4,4 +4,28 @@ const myApp = {
   BASE_URL: 'http://localhost:3000',
 };
 
-$(document).ready(() => {});
+$('#sign-up').on('submit', function(e) {
+
+
+
+let signUp = function(e) {
+  e.preventDefault();
+  let formData = new FormData(e.target);
+  $.ajax({
+    url: myApp.BASE_URL + '/sign-up',
+    method: 'POST',
+    contentType: false,
+    processData: false,
+    data: formData,
+  }).done(function(data) {
+    console.log(data);
+  }).fail(function(jqxhr) {
+    console.error(jqxhr);
+  });
+ };
+});
+
+
+module.exports = {
+  signUp,
+};
