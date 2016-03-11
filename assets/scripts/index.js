00ef6252edfc0ca971eb52133a689eff3040c65b
kpie89 <kpie89@gmail.com>
2016-03-11 16:35:36 -0500

    add event listeners for user actions

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,3 +5,16 @@
 
 // use require without a reference to ensure a file is bundled
 require('./example');
+
+const myApp = {
+  BASE_URL: 'https://localhost:3000/',
+};
+
+const userActions = require('./user-actions.js');
+
+$(document).ready(() => {
+  $('#sign-up').on('submit', userActions.signUp);
+  $('#sign-in').on('submit', userActions.signIn);
+  $('#sign-out').on('submit', userActions.signOut);
+  $('#change-password').on('submit', userActions.changePassword);
+});
