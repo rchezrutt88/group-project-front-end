63cfe609b85e4a9d87401b6c58ed736df640dba0
kpie89 <kpie89@gmail.com>
2016-03-14 14:55:45 -0400

    add AJAX request for entry create

---
+++ assets/scripts/entry-actions.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const myApp = require('./user-actions').myApp;
+
+let createEntry = function(e) {
+  e.preventDefault();
+  let formData = new FormData(e.target);
+  $.ajax({
+    url: myApp.BASE_URL + '/entries',
+    method: 'POST',
+    headers: {
+      Authorization: 'Token token=' + myApp.user.token,
+    },
+    contentType: false,
+    processData: false,
+    data: formData,
+  }).done(function(data) {
+    console.log(data);
+  }).fail(function(jqxhr) {
+    console.error(jqxhr);
+  });
+};
+
+module.exports = {
+  createEntry
+};
