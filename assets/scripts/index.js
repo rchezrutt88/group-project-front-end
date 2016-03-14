4315c059299bd0cc7db8c94b3f5fb00d5c8ef28e
Dumplingpuff <jason.liang2012@gmail.com>
2016-03-14 16:34:58 -0400

    Created handlebars function

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -6,7 +6,7 @@
 // use require without a reference to ensure a file is bundled
 require('./example');
 
-const myApp = require('./my-app')
+const myApp = require('./my-app');
 
 const userActions = require('./user-actions.js');
 const entryActions = require('./entry-actions');
@@ -15,10 +15,10 @@ const page = require('./page-actions.js');
 $(document).ready(() => {
   $('#sign-up').on('submit', userActions.signUp);
   $('#sign-in').on('submit', userActions.signIn);
-  $('#sign-out-button').on('click', userActions.signOut);
+  $('#sign-out').on('submit', userActions.signOut);
   $('.page2-sign-out').on('click', userActions.signOut);
   $('#change-password').on('submit', userActions.changePassword);
   $('#create-page-modal').on('submit', page.pageCreate);
-  $('#entry-view').on('submit', entryActions.createEntry)
+  $('#entry-view').on('submit', entryActions.createEntry);
   $('#page2').hide();
 });
