d38cfe22fe68df75208cd54a78728337d4f40cd0
kpie89 <kpie89@gmail.com>
2016-03-15 09:31:49 -0400

    temp put entry POST AJAX in user actions

--- assets/scripts/user-actions.js
+++ assets/scripts/user-actions.js
@@ -91,9 +91,29 @@ let signOut = function(e) {
   });
 };
 
+let createEntry = function(e) {
+  e.preventDefault();
+  let formData = new FormData(e.target);
+  $.ajax({
+    url: myApp.BASE_URL + '/entries',
+    method: 'POST',
+    headers: {
+      Authorization: 'Token token=' + myApp.user.token,
+    },
+    contentType: false,
+    processData: false,
+    data: formData,
+  }).done(function(data) {
+    console.log(data);
+  }).fail(function(jqxhr) {
+    console.error(jqxhr);
+  });
+};
+
 module.exports = {
   signUp,
   signIn,
   changePassword,
-  signOut
+  signOut,
+  createEntry
 };
